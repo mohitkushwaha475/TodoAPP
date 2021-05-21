6278433d093fe2b9408bfa67abc77f8d7b2959b7
Mohit <mohitkushwaha9899@gmail.com>
2021-05-21 14:45:42 +0530

    Alert updated for todo length less than 3

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList ,Alert} from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/TodoItem';
 import AddTodo from './components/addTodo';
@@ -20,12 +20,22 @@ export default function App() {
    }
 
    const submitHandler=(text)=>{
+     if (text.length>3){
     setTodos((prevTodos)=>{
       return [
         {text: text,key:Math.random().toString()},
         ...prevTodos
       ];
     })
+  }
+  else{
+   Alert.alert('OOPS!','Todo must be over 3 chars long ',[
+     {text:'understand',onPress: ()=> console.log('alert closed')}
+
+   ])
+  }
+
+
   }
 
 
